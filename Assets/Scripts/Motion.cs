f25e853b03263dfa8842829afb3e5ea648955442
gravityfalls0116 <disneyfrozen0116@gmail.com>
2021-04-03 23:39:57 +0900

    First Upload
    -AI를 본격적으로 개발하기 전, 유니티 엔진에 대해서 공부하기 위해 간단한 1인칭 시점 게임 예제를 만들어 보았습니다.
    
    -1인칭 시점의 플레이어, WASD 조작, 마우스로 시점 이동, 점프를 구현했습니다.

---
+++ Assets/Scripts/Motion.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Com.Sungjin.FPSpractice
+{
+
+
+    public class Motion : MonoBehaviour
+    {
+        public float speed;
+        public float sprintModifier;
+        public Camera normalCam;
+        public Transform groundDetector;
+        public LayerMask ground;
+        public float jumpForce;
+
+        private Rigidbody rig;
+        private float baseFOV;
+        private float sprintFOVModifier = 1.5f;
+
+
+        void Start()
+        {
+            baseFOV = normalCam.fieldOfView;
+            Camera.main.enabled = false;
+            rig = GetComponent<Rigidbody>();
+        }
+
+
+        void FixedUpdate()
+        {
+            //Axis
+            float t_hmove =Input.GetAxisRaw("Horizontal");
+            float t_vmove = Input.GetAxisRaw("Vertical");
+
+            //Controls
+            bool sprint = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+            bool jump = Input.GetKey(KeyCode.Space);
+
+            //States
+            bool isGrounded = Physics.Raycast(groundDetector.position, Vector3.down, 0.1f, ground);
+            bool isJumping = jump && isGrounded;
+            bool isSprinting = sprint && t_vmove > 0 && !isJumping && isGrounded;
+ 
+
+            //Jumping
+            if (isJumping)
+            {
+                rig.AddForce(Vector3.up * jumpForce);
+            }
+
+            //Movements
+            Vector3 t_direction = new Vector3(t_hmove,0, t_vmove);
+            t_direction.Normalize();
+
+            float t_adjustSpeed = speed;
+            if (isSprinting) t_adjustSpeed *= sprintModifier;
+
+            
+            Vector3 t_targetVelocity= transform.TransformDirection(t_direction) * t_adjustSpeed * Time.deltaTime;
+            t_targetVelocity.y = rig.velocity.y;
+            rig.velocity = t_targetVelocity;
+
+            //Field of View
+            if(isSprinting) {normalCam.fieldOfView = Mathf.Lerp(normalCam.fieldOfView,baseFOV*sprintFOVModifier,Time.deltaTime*8f); }
+            else { normalCam.fieldOfView = Mathf.Lerp(normalCam.fieldOfView, baseFOV, Time.deltaTime * 8f); ; }
+        }
+    }
+}
